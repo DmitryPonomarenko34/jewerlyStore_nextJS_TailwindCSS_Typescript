9d7d2d87ebb653bff1431eab3368ebc181f19f05
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-04-19 18:10:03 +0300

    added fonts & variable to next and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,14 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+     fontFamily: {
+        ttricordi: 'var(--font-ttricordi)',
+        raleway: 'var(--font-raleway)',
       },
     },
   },
